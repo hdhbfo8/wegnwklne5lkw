d149bc18fec92522dcf3fcc7b394d42a8922684e
hdhbfo8 <Zxz89519432433@gmail.com>
2025-11-25 12:53:26 +0500

    Add files via upload

---
+++ homework 17.py
@@ -0,0 +1,26 @@
+user = int(input("Введите число: "))
+
+if user > 100:
+    print("Число больше 100")
+elif user > 50:
+    print("Число больше 50, но меньше 100")
+else:
+    print("Число меньше 50")
+    
+    
+age = int(input("Возраст: "))
+
+if age >= 18:
+    print("Входи")
+else:
+    print ("Только для совершеннолетних")
+    
+    
+a = int(input("Введите число: "))
+b = int(input("Введите число: "))
+
+if a < b:
+    print(a)
+else:
+    print(b)
+    
