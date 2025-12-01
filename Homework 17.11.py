541482ab8de396dfaab597e43aa610a6c55762e1
hdhbfo8 <Zxz89519432433@gmail.com>
2025-12-01 20:28:56 +0500

    Add files via upload

---
+++ Homework 17.11.py
@@ -0,0 +1,18 @@
+sum = 0
+
+# Цикл от 1 до 100 включительно
+for i in range(1, 101):
+    sum += i
+
+print(sum)
+
+
+for number in range(1, 101):
+    if number % 2 == 0:
+        print(number)
+
+
+for number in range(1, 301):
+    if number % 2 == 1:
+        print(number)
+        
